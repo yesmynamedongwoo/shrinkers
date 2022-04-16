4be62afe16ec54bf88ce7d50ba73cb8f77c667c8
yesmynamedongwoo <kdw3917ss@gmail.com>
2022-04-17 08:03:35 +0900

    Initial commit

---
+++ shortener/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User as U
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class PayPlan(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+    updated_at = models.DateTimeField(auto_now=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+
+class Users(AbstractUser):
+    pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING)
+
+class UserDetail(models.Model):
+    user = models.OneToOneField(Users,on_delete=models.CASCADE)
+    pay_plan = models.ForeignKey(PayPlan,on_delete=models.DO_NOTHING)
